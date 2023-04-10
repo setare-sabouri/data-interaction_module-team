32622249aff6cfe170b86b14d773cefd70b3d938
setare-sabouri <100029656+setare-sabouri@users.noreply.github.com>
2023-04-10 02:17:40 +0200

    made sequlize and server folders seperate

--- server.js
+++ server.js
@@ -3,26 +3,18 @@ const tasksRouter = require('./routes/task.routs');
 require("dotenv").config();
 const app = express();
 const port = 3000;
-const db = require("./models/index");
+const sequelize = require("./models/index");
 
 const main = async () => {
 	try {
-		await db.sequelize.authenticate();
+		await sequelize.authenticate();
 		console.log("Connection has been established successfully.");
 	} catch (error) {
 		console.error("Unable to connect to the database:", error);
 	}
 };
+
 app.use(tasksRouter);
-
-
-db.sequelize.sync()
-	.then(() => {
-		console.log("Synced db.");
-	})
-	.catch((err) => {
-		console.log("Failed to sync db: " + err.message);
-	});
 
 app.get("/", (req, res) => {
 	res.send("Welcome");
@@ -31,7 +23,5 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
 });
-
-
 
 main();
